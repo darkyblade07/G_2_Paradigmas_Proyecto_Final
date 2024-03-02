360ea0544a6d1d0ec5239b73c0526a4dcd976ba7
Santiago <santiagomarinherrera09@gmail.com>
2024-03-02 17:35:12 -0600

    Adjusting code

--- app.py
+++ app.py
@@ -4,13 +4,20 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = 'Files'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+ALLOWED_EXTENSIONS = set()
+app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
+
+# Función para verificar si la extensión es permitida
+def allowed_file(filename):
+    return True
 
 # Página principal
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', confirmation=None)
 
 # Ruta para manejar la carga de archivos
 @app.route('/upload', methods=['POST'])
@@ -22,29 +29,39 @@ def upload_file():
 
     if file.filename == '':
         return redirect('/')
+
+    if file and allowed_file(file.filename):
+        # Verifica y crea la carpeta 'uploads'
+        if not os.path.exists(UPLOAD_FOLDER):
+            os.makedirs(UPLOAD_FOLDER)
+        
+        # Guarda el archivo en la carpeta 'uploads'
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        # Mover el archivo a la carpeta principal si no tiene extensión
+        if '.' not in filename:
+            result_file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        else:
+            # Determinar la extensión del archivo
+            file_extension = filename.rsplit('.', 1)[1].lower()
+
+            # Determinar la carpeta de destino según la extensión
+            destination_folder = os.path.join(app.config['UPLOAD_FOLDER'], file_extension)
+            if not os.path.exists(destination_folder):
+                os.makedirs(destination_folder)
+
+            # Mover el archivo a la carpeta de destino
+            result_file_path = os.path.join(destination_folder, filename)
+
+        os.rename(file_path, result_file_path)
+
+        confirmation_message = f'Sucessfully uploaded. File location: {result_file_path}.'
     
-    # Verifica y crea la carpeta 'uploads'
-    if not os.path.exists(UPLOAD_FOLDER):
-        os.makedirs(UPLOAD_FOLDER)
-    
-    # Guarda el archivo en la carpeta 'uploads'
-    filename = secure_filename(file.filename)
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    file.save(file_path)
-
-    # Determinar la extensión del archivo
-    file_extension = filename.rsplit('.', 1)[1].lower()
-
-    # Determinar la carpeta de destino según la extensión
-    destination_folder = os.path.join(app.config['UPLOAD_FOLDER'], file_extension)
-    if not os.path.exists(destination_folder):
-        os.makedirs(destination_folder)
-
-    # Mover el archivo a la carpeta de destino
-    result_file_path = os.path.join(destination_folder, filename)
-    os.rename(file_path, result_file_path)
-
-    return f'File uploaded successfully. Result saved as {result_file_path}.'
+        return render_template('index.html', confirmation=confirmation_message)
+    else:
+        return render_template('index.html', confirmation='Invalid file extension. Only txt, pdf, png, jpg, jpeg, gif, csv, json, sql are allowed.')
 
 if __name__ == '__main__':
     app.run(debug=True)
